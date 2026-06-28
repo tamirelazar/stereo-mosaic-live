8be1f3a83325587997d16220a3e06c0aed388e37
Tamir Elazar <60173095+tamirelazar@users.noreply.github.com>
2026-06-29 00:42:41 +0300

    chore: drop dead make_panorama entry, fix test name and newline

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -2,7 +2,7 @@ import numpy as np
 from smlive import utils
 
 
-def test_read_image_returns_grayscale_float():
+def test_blur_spatial_preserves_shape_and_dtype():
     # build_gaussian_pyramid + blur_spatial are the carried primitives align depends on
     im = np.random.rand(64, 64).astype(np.float64)
     blurred = utils.blur_spatial(im, 3)
